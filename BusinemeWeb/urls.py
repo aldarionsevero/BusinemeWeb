e7dcf9dca725dd33251da7e3a39371066989f913
Lucas <lucassalves65@gmail.com>
2015-04-17 03:47:36 -0300

    Some files relocated. Added url mappings and controllers for home (user will be registered in home).
    Signed-off-by: Lucas <lucassalves65@gmail.com>

--- BusinemeWeb/urls.py
+++ BusinemeWeb/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'BusinemeWeb.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       # Examples:
+                       # url(r'^$', 'BusinemeWeb.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'^home/', 'controllers.home.home'),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
