d7e7706e2fb680a4f9ec557c008e08555054cf64
msfernandes <matheus.souza.fernandes@gmail.com>
2015-04-20 23:31:38 -0300

    Login

--- BusinemeWeb/urls.py
+++ BusinemeWeb/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'BusinemeWeb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', 'controllers.main.main'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- configuration/apps.py
+++ configuration/apps.py
@@ -28,6 +28,6 @@ PLUGINS_APPS = (
 )
 
 # Busine.me application apps
-BUSINEME_APPS = ()
+BUSINEME_APPS = ('controllers',)
 
 INSTALLED_APPS = DJANGO_APPS + PLUGINS_APPS + BUSINEME_APPS
