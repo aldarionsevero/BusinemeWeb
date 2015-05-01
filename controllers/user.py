225f80d3f2c9f7e13bad14e323561e2b0e5e97c6
Lucas <lucassalves65@gmail.com>
2015-05-01 00:44:17 -0300

    Fixed several links to action urls arround the code. Implemented the controller for the user change of password. Still needs to return better feedback messages to the user.
    Signed-off-by: Lucas <lucassalves65@gmail.com>

--- controllers/user.py
+++ controllers/user.py
@@ -7,6 +7,7 @@ from django.template import RequestContext
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
+from django.http import HttpResponse
 
 
 def register_user_page(request):
@@ -55,9 +56,10 @@ def log_user(request):
 @login_required
 def logout_user(request):
     logout(request)
-    return redirect('/login/')
+    return redirect('/login/', context_instance=RequestContext(request))
 
 
+@login_required
 def user_account(request):
     if request.user.is_authenticated():
         user = request.user
@@ -67,9 +69,40 @@ def user_account(request):
                               context_instance=RequestContext(request))
 
 
-# def change_password(request):
-#     return render_to_response('change_password_page.html', {'user': user},
-#                               context_instance=RequestContext(request))
+@login_required
+def change_password_page(request):
+    if request.user.is_authenticated():
+        user = request.user
+    else:
+        user = None
+    return render_to_response('change_password_page.html', {'user': user},
+                              context_instance=RequestContext(request))
+
+
+@login_required
+def change_password(request):
+    if request.user.is_authenticated():
+        user = request.user
+    else:
+        user = None
+
+    old_password = request.POST["old_password"]
+    new_password1 = request.POST["new_password1"]
+    new_password2 = request.POST["new_password2"]
+
+    if not user.check_password(old_password):
+        response = HttpResponse("Sua senha antiga está incorreta.")
+    else:
+        if not (new_password1 == new_password2):
+            response = HttpResponse("Sua senha nova não bate com a comparação")
+        else:
+            user.set_password(new_password1)
+            user.save()
+            response = redirect("/login/",
+                                context_instance=RequestContext(request))
+            logout(request)
+    return response
+
 
 def change_userdata(request):
     # Try to change the data of the usere
@@ -79,5 +112,5 @@ def change_userdata(request):
         user.email = request.POST["email"]
         user.username = request.POST["username"]
         user.save()
-    return redirect("/perfil/",
+    return redirect("/",
                     context_instance=RequestContext(request))
