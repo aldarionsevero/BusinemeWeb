8451e11ce69761ae15923432cad09daf7c392427
Luiz Oliveira <ziuloliveira@gmail.com>
2015-05-06 23:46:33 -0300

    Starting tests
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

---
+++ api/tests/test_busiline.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from api.busline import BuslineAPI
+
+
+from django.test import SimpleTestCase
+
+
+class testBusilineAPI(SimpleTestCase):
+
+    """docstring for testBusilineAPI"""
+
+    def testnoob(self):
+        self.assertIsNone(None)
