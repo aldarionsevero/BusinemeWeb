ad203f43dde33d5164d7b083ddab83136f097bfb
Macário Soares <macario.junior@gmail.com>
2015-05-12 00:52:08 -0300

    Starting lettuce use with splinter

--- controllers/features/search_busline.feature
+++ controllers/features/search_busline.feature
@@ -7,6 +7,6 @@ Feature: Search busline
         Given I am on Busine.me home page
         And I type "205" in the field search lines
         When I press "Search"
-        Then I should see "Results"
-        And I should see "We found '2' buslines that contains '205' in their numbers"
-        And I should see the list with the found bulines.
+        Then I should see "Resultados"
+        And I should see "0.205 \n Rota:Gama Sul-Leste/"M" Norte ( Comercial) \n VIA ACES. HRG/OESTE - GAMA \n Percurso: 45.78 KM \n Tarifa: R$3.0"
+        
--- controllers/features/steps.py
+++ controllers/features/steps.py
@@ -1,21 +1,32 @@
 from lettuce import *
 from lettuce.django import django_url
 from lxml import html
-from django.test.client import Client
+#from django.test.client import Client
 from nose.tools import assert_equals
-#from splinter.browser import Browser
+from splinter.browser import Browser
 
 @before.all
 def set_browser():
-    world.browser = Client()
+    world.browser = Browser('zope.testbrowser')
 
 
 @step('I am on Busine.me home page')
 def i_am_on_busineme_home_page(step):
     name = "/"
     full_url = django_url(name)
-    world.browser.response = world.browser.get(full_url)
+    world.browser.response = world.browser.visit(full_url)
 
 @step('I type "(.*)" in the field search lines')
 def i_type_value_in_the_field_search_lines(step, value):
-    pass
+    world.browser.fill("busline", value)
+
+@step('When I press "(.*)"')
+def when_i_press_search(step,button):
+    world.browser.find_by_name('pesquisar').first.click()
+
+@step('Then I should see "(.*)"')
+def then_i_should_see(step, value):
+    world.browser.find_by_css('h1').first
+@step('And I should see "(.*)"')
+def and_i_should_see(step, value):
+    world.browser.find_by_css('h3').first.value
