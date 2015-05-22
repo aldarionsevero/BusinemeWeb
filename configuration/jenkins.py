c94357d50b6c1882e0301a556adf2824613b1e32
Lucas <lucassalves65@gmail.com>
2015-05-22 03:46:26 -0300

    Removed csslint from the django-jenkins tasks
    Signed-off-by: Lucas <lucassalves65@gmail.com>

--- configuration/jenkins.py
+++ configuration/jenkins.py
@@ -4,6 +4,6 @@ JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.run_pyflakes',
     # 'django_jenkins.tasks.run_jslint',
-    'django_jenkins.tasks.run_csslint',
+    # 'django_jenkins.tasks.run_csslint',
     'django_jenkins.tasks.run_sloccount',
 )
