19ef7e3c16fa1e41d972ed578a6d158a99f3d366
Anna Larissa Martins <annalarissa.martins@hotmail.com>
2015-05-26 23:38:40 -0300

    New tests for busline_controller.py
    Signed-off-by: Emanuel Holanda <manusenac@gmail.com>
    Signed-off-by: Anna Larissa Martins <annalarissa.martins@hotmail.com>
    Signed-off-by: Halê Valênte <halegal@gmail.com>
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>
    Signed-off-by: Gustavo Sabino <gustavo.sabino@hotmail.com>

--- controllers/busline_controller.py
+++ controllers/busline_controller.py
@@ -9,24 +9,13 @@ from controllers.utils import error_message
 
 def search_line(request):
     line_number = request.GET['busline']
-    if len(line_number) == 1:
-
-        # response = redirect(
-        #     request.META['HTTP_REFERER'],
-        #     context_instance=RequestContext(request))
-        response = error_message(
-            "Erro :(", "Busca com apenas um dígito", "A busca deve ser \
-                realizada com no mínimo 2 dígitos ou apenas \
-        vazia para vizualizar todas as linhas.",
-            "search_result_page.html", request)
-    else:
-        buslines = Busline.filter_by_line_number(line_number)
-        count_busline = len(buslines)
-        response = render_to_response("search_result_page.html",
-                                      {'buslines': buslines,
-                                          'count_busline': count_busline,
-                                          'searched_number': line_number},
-                                      context_instance=RequestContext(request))
+    buslines = Busline.filter_by_line_number(line_number)
+    count_busline = len(buslines)
+    response = render_to_response("search_result_page.html",
+                                  {'buslines': buslines,
+                                   'count_busline': count_busline,
+                                   'searched_number': line_number},
+                                  context_instance=RequestContext(request))
 
     return response
 
--- controllers/tests/test_BuslineController.py
+++
@@ -1,6 +0,0 @@
-from django.test import SimpleTestCase, Client
-from models.busline import Busline
-
-
-STATUS_OK = 200
-STATUS_REDIRECT = 302
