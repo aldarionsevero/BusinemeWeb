bd547ed2c0972306110d5b67d47a6ee06810e6c6
Lucas <lucassalves65@gmail.com>
2015-05-27 02:50:57 -0300

    All tests for user registration and validations
    Signed-off-by: Lucas <lucassalves65@gmail.com>

--- controllers/tests/test_user.py
+++ controllers/tests/test_user.py
@@ -36,12 +36,52 @@ class UserControllerTest(SimpleTestCase):
         data = {'password': '4321'}
         return data
 
-    def register_user_duplicate_email(self):
-        data1 = register_post_data()
+    def test_register_user_duplicate_email(self):
+        data1 = {'name': 'test_user1',
+                 'email': 'same@email.com',  # same email
+                 'username': 'test_user1',
+                 'password': '1234'}
         data2 = {'name': 'test_user2',
-                 'email': 'test@email.com',
+                 'email': 'same@email.com',  # same email
                  'username': 'test_user2',
                  'password': '1234'}
+        response1 = self.client.post('/cadastrar/usuario/', data1, follow=True)
+        response2 = self.client.post('/cadastrar/usuario/', data2, follow=True)
+
+        self.assertEquals(
+            response1.redirect_chain, [('http://testserver/login/', 302)])
+        self.assertEquals(
+            response2.redirect_chain, [])
+        self.assertEquals(response1.status_code, STATUS_OK)
+        self.assertEquals(response2.status_code, STATUS_OK)
+
+    def test_register_user_invalid_email(self):
+        data = {'name': 'test_user2',
+                'email': 'testemail.com',
+                'username': 'test_user2',
+                'password': '1234'}
+        response = self.client.post('/cadastrar/usuario/', data, follow=True)
+        self.assertEquals(response.redirect_chain, [])
+        self.assertEquals(response.status_code, STATUS_OK)
+
+    def test_register_user_invalid_username(self):
+        data1 = {'name': 'test_user1',  # same name
+                 'email': 'test1@email.com',
+                 'username': 'test_user_same',
+                 'password': '1234'}
+        data2 = {'name': 'test_user2',  # same name
+                 'email': 'test2@email.com',
+                 'username': 'test_user_same',
+                 'password': '1234'}
+        response1 = self.client.post('/cadastrar/usuario/', data1, follow=True)
+        response2 = self.client.post('/cadastrar/usuario/', data2, follow=True)
+
+        self.assertEquals(
+            response1.redirect_chain, [('http://testserver/login/', 302)])
+        self.assertEquals(
+            response2.redirect_chain, [])
+        self.assertEquals(response1.status_code, STATUS_OK)
+        self.assertEquals(response2.status_code, STATUS_OK)
 
     def test_register_user_page(self):
         response = self.client.get('/cadastro/')
@@ -101,33 +141,9 @@ class UserControllerTest(SimpleTestCase):
     # FIX-ME
     def test_register_user_page_feed(self):
         self.create_user()
-        self.client.login(username='test_username', password='test_password')
+        self.client.login(
+            username='test_username', password='test_password')
         response = self.client.get('/')
         self.assertEquals(response.status_code, STATUS_OK)
         self.client.logout()
         self.user.delete()
-
-    # def test_register_user_invalid_email(self):
-    #     data = {'name': 'test_user',
-    #             'email': 'testemail.com',
-    #             'username': 'test_user',
-    #             'password': '1234'}
-    #     response = self.client.post('/cadastrar/usuario/', data)
-    #     htmlvarsteste = {"Erro :(",
-    #                      "E-mail invalido.",
-    #                      "Verifique se a escrita. O email deve conter @ e . (ponto)."}
-    #     self.assertEquals(response.htmlvars, htmlvarsteste)
-
-    # FIX-ME
-    # def test_register_user_invalid_username(self):
-    #     data2 = {'name': 'test_user2',
-    #              'email': 'teste2@mail.com',
-    #              'username': 'test_user',
-    #              'password': '1234'}
-    #     data = self.register_post_data()
-    #     self.client.post('/cadastrar/usuario/', data),
-    #     response2 = self.client.post('/cadastrar/usuario/', data2),
-    #     htmlvarstest = {"Erro :(",
-    #                     "Usuário ja cadastrado.",
-    #                     "O nome de usuario escolhido ja esta em uso ."}
-    #     self.assertEquals(response2.htmlvars, htmlvarstest)
