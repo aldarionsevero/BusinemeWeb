670e06a79f3257ee9345b388bd7283b388ed1574
Parley Martins <parleypachecomartins@gmail.com>
2015-05-30 16:30:03 -0300

    Add tests to filter by line and description

--- api/tests/test_busiline_api.py
+++ api/tests/test_busiline_api.py
@@ -16,18 +16,12 @@ class testBusilineAPI(SimpleTestCase):
     def setUp(self):
         self.api = BuslineAPI()
 
-        self.buslines = [
-            self.create_busline(),
-            self.create_busline(line_number="002"),
-            self.create_busline(line_number="003")]
-
     @patch('api.busline.requests.get')
     def test_all(self, mock_get):
+        mock_get = mock_get
         mock_response = Mock()
-        expected = self.all_buslines_dict()
-        mock_response.json.return_value = expected
+        mock_response.json.return_value = self.all_buslines_dict()
         mock_get.return_value = mock_response
-
         all_url = "http://localhost:8080/api/v1/busline/"
 
         all_buslines = self.api.all()
@@ -35,18 +29,31 @@ class testBusilineAPI(SimpleTestCase):
         mock_get.assert_called_once_with(all_url)
         mock_response.json.assert_called_once()
 
-    def create_busline(self, line_number="001", description="Test description",
-                       via="Via someplace", route_size=0.0, fee=0.0, company=None,
-                       terminals=[]):
-        busline = Busline()
-        busline.line_number = line_number
-        busline.description = description
-        busline.via = via
-        busline.route_size = route_size
-        busline.fee = fee
-        busline.company = company
-        busline.terminals = terminals
-        return busline
+    @patch('api.busline.requests.get')
+    def test_filter_by_line(self, mock_get):
+        mock_get = mock_get
+        mock_response = Mock()
+        mock_response.json.return_value = self.buslines_by_number()
+        mock_get.return_value = mock_response
+        all_url = "http://localhost:8080/api/v1/busline/?line_number__contains=17&limit=0"
+
+        all_buslines = self.api.filter_by_line('17')
+
+        mock_get.assert_called_once_with(all_url)
+        mock_response.json.assert_called_once()
+
+    @patch('api.busline.requests.get')
+    def test_filter_by_description(self, mock_get):
+        mock_get = mock_get
+        mock_response = Mock()
+        mock_response.json.return_value = self.buslines_by_description()
+        mock_get.return_value = mock_response
+        all_url = "http://localhost:8080/api/v1/busline/?description__contains=setor&limit=0"
+
+        all_buslines = self.api.filter_by_description('setor')
+
+        mock_get.assert_called_once_with(all_url)
+        mock_response.json.assert_called_once()
 
     def all_buslines_dict(self):
         return {
@@ -101,6 +108,61 @@ class testBusilineAPI(SimpleTestCase):
                         {"address": "null", "description": "APAED", "id": 59,
                          "resource_uri": "/api/v1/terminal/59"}
                 ], "via": "VIA M3 - CEILÃ‚NDIA"},
+                {"company": {"id": 10, "name": "VIAÃ‡ÃƒO CIDADE BRASÃLIA", "resource_uri": "/api/v1/company/10"},
+                 "description": "Setor \"O\" (via M-2)/SAAN(Estrutural)", "fee": 3.0, "id": 380,
+                 "line_number": "0.917", "resource_uri": "/api/v1/busline/380", "route_size": 32.17, "terminals": [
+                    {"address": "null", "description": "SETOR \"O\"",
+                        "id": 8, "resource_uri": "/api/v1/terminal/8"},
+                    {"address": "null", "description": "FINAL DA QUADRA 02 DO SAAN",
+                        "id": 117, "resource_uri": "/api/v1/terminal/117"}
+                ], "via": "VIA O4 - CEILÃ‚NDIA"}
+            ]}
+
+    def buslines_by_number(self):
+        return {
+            "meta": {"limit": 1000, "next": "null", "offset": 0, "previous": "null", "total_count": 6},
+            "objects": [
+                {"company": {"id": 7, "name": "SÃƒO JOSÃ‰", "resource_uri": "/api/v1/company/7"},
+                 "description": "Riacho Fundo/RodoviÃ¡ria do Plano Piloto (Eixo)", "fee": 2.0, "id": 76,
+                 "line_number": "0.172", "resource_uri": "/api/v1/busline/76", "route_size": 22.57,
+                 "terminals": [
+                    {"address": "null", "description": "RIACHO FUNDO",
+                        "id": 6, "resource_uri": "/api/v1/terminal/6"},
+                    {"address": "null", "description": "RODOVIÃRIA DO PLANO PILOTO",
+                        "id": 19, "resource_uri": "/api/v1/terminal/19"}
+                ], "via": "AV. RIACHO FUNDO I SUL"},
+                {"company": {"id": 7, "name": "SÃƒO JOSÃ‰", "resource_uri": "/api/v1/company/7"},
+                 "description": "Riacho Fundo/RodoviÃ¡ria do Plano Piloto  (Eixo)", "fee": 2.0, "id": 557,
+                 "line_number": "172.1", "resource_uri": "/api/v1/busline/557", "route_size": 26.48,
+                 "terminals": [
+                    {"address": "null", "description": "RIACHO FUNDO",
+                        "id": 6, "resource_uri": "/api/v1/terminal/6"},
+                    {"address": "null", "description": "RODOVIÃRIA DO PLANO PILOTO",
+                        "id": 19, "resource_uri": "/api/v1/terminal/19"}
+                ], "via": "AV. RIACHO FUNDO I SUL"},
+                {"company": {"id": 62, "name": "RÃPIDO BRASILIA", "resource_uri": "/api/v1/company/62"},
+                 "description": "Escola Classe 405 Sul/ASTECA (ISM)", "fee": 0.0, "id": 558,
+                 "line_number": "172.2", "resource_uri": "/api/v1/busline/558", "route_size": 25.84,
+                 "terminals": [
+                    {"address": "null", "description": "E.C. 405 SUL",
+                        "id": 78, "resource_uri": "/api/v1/terminal/78"},
+                    {"address": "null", "description":
+                        "ASTECA (ISM)", "id": 129, "resource_uri": "/api/v1/terminal/129"}
+                ], "via": "VIA EQS 206/205 COMÃ‰RCIO LOCAL SUL"},
+                {"company": {"id": 7, "name": "SÃƒO JOSÃ‰", "resource_uri": "/api/v1/company/7"},
+                 "description": "Riacho Fundo/RodoviÃ¡ria do Plano Piloto (Eixo-Noturna)", "fee": 2.0, "id": 559,
+                 "line_number": "172.4", "resource_uri": "/api/v1/busline/559", "route_size": 22.57, "terminals": [
+                    {"address": "null", "description": "RIACHO FUNDO",
+                        "id": 6, "resource_uri": "/api/v1/terminal/6"},
+                    {"address": "null", "description": "RODOVIÃRIA DO PLANO PILOTO",
+                        "id": 19, "resource_uri": "/api/v1/terminal/19"}
+                ], "via": "AV. RIACHO FUNDO I SUL"}
+            ]}
+
+    def buslines_by_description(self):
+        return {
+            "meta": {"limit": 1000, "next": "null", "offset": 0, "previous": "null", "total_count": 6},
+            "objects": [
                 {"company": {"id": 10, "name": "VIAÃ‡ÃƒO CIDADE BRASÃLIA", "resource_uri": "/api/v1/company/10"},
                  "description": "Setor \"O\" (via M-2)/SAAN(Estrutural)", "fee": 3.0, "id": 380,
                  "line_number": "0.917", "resource_uri": "/api/v1/busline/380", "route_size": 32.17, "terminals": [
