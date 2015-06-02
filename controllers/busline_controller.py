d6ccc1283e978ea99b34fd9c6a4a27a84347c50f
Halê Valente Silva <halegal@gmail.com>
2015-06-02 13:35:43 -0300

    busline controller smells fixed

--- controllers/busline_controller.py
+++ controllers/busline_controller.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+""" Busline controller docstring """
 from django.shortcuts import render_to_response
 from models.busline import Busline
 # from api import BuslineAPI
@@ -9,7 +10,7 @@ from controllers.utils import error_message
 
 def search_line(request):
     """
-    Performs a search for bus lines that contain the input value entered
+    Perform a search for bus lines that contain the input value entered\
     by the user then returns the result page and the list of results.
     """
     line_number = request.GET['busline']
@@ -25,15 +26,17 @@ def search_line(request):
 
 def advanced_search_line(request):
     """
-    Performs an advanced search for bus lines which contain the input values
+    Perform an advanced search for bus lines which contain the input values\
     entered by the user then returns the result page and the list of results.
     """
     if ((len(request.GET['busline']) < 2) and
             (len(request.GET['description']) < 2) and
             (len(request.GET['terminal__description']) < 2)):
         response = error_message(
-            "Erro :(", "Entrada inválida.", "Os campos preenchidos da busca avançada devem possuir \
-                no mínimo 2 dígitos.",
+            "Erro :(",
+            "Entrada inválida.",
+            "Os campos preenchidos da busca avançada devem possuir \
+            no mínimo 2 dígitos.",
             "search_result_page.html", request)
     else:
         buslines = Busline.filter_by_multiple(
@@ -52,8 +55,6 @@ def advanced_search_line(request):
 
 
 def advanced_search_page(request):
-    """
-    Returns the advanced search page when requested.
-    """
+    """ Return the advanced search page when requested. """
     return render_to_response("advanced_search_busline.html",
                               context_instance=RequestContext(request))
