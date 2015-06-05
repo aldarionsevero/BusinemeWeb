55093265ff976a021696ab45006d5eb936b265f5
Lucas <lucassalves65@gmail.com>
2015-06-05 20:38:28 -0300

    the rest of user docstrings
    Signed-off-by: Lucas <lucassalves65@gmail.com>

--- models/user.py
+++ models/user.py
@@ -14,29 +14,35 @@ class User(DjangoUser):
 
     @classmethod
     def all(cls):
+        """Returns all users"""
         return cls.objects.all()
 
     @classmethod
     def filter_by_username(cls, username):
+        """Returns all users containing specified username"""
         return cls.objects.filter(username=username)
 
     @classmethod
     def filter_by_email(cls, email):
+        """Returns all users containing specified email"""
         return cls.objects.filter(email=email)
 
     def validate_user_name(self):
+        """Validates existing username"""
         if User.filter_by_username(self.username) is None:
             return True
         else:
             return False
 
     def validate_email(self):
+        """Validates email regex"""
         if len(self.email) > 6:
             if re.match(r'\w[\w\.-]*@\w[\w\.-]+\.\w+', self.email) is not None:
                 return True
         return False
 
     def validate_unique_email(self):
+        """Validates unique email"""
         users = User.filter_by_email(self.email)
         if len(users) == 0:
             return True
@@ -44,6 +50,7 @@ class User(DjangoUser):
             return False
 
     def validade_user_password(self, userpassword):
+        """Validates password not blank"""
         if len(userpassword) == 0:
             return False
         else:
