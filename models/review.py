211dcbfc066a57b39e436d8bc802e2cc1986f795
Gustavo Sabino <gustavo.sabino@hotmail.com>
2015-06-10 18:06:38 -0300

    Making review modal

--- models/review.py
+++ models/review.py
@@ -6,7 +6,6 @@ class Review(models.Model):
     date = models.DateField(auto_now=True)
     time = models.TimeField(auto_now=True)
     review_type = models.BooleanField(default=False)
-    # Belive thats a text, but if it's a "likeButton", please rectify
 
     def __unicode__(self):
         """Return comment of the review."""
