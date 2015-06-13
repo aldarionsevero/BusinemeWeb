5addd0e5cf14d161002db79f9c68c43ee08a7736
Halê Valente Silva <halegal@gmail.com>
2015-06-13 12:05:34 -0300

    Add use to post.
    Signed-off-by: Parley Martins <parleypachecomartins@gmail.com>
    Signed-off-by: Halê Valente Silva <halegal@gmail.com>

--- models/post.py
+++ models/post.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from busline import Busline
+from user import User
 
 
 class Post(models.Model):
@@ -15,11 +16,12 @@ class Post(models.Model):
     busline = models.ForeignKey(Busline)
     date = models.DateField(auto_now=True)
     time = models.TimeField(auto_now=True)
+    user = models.ForeignKey(User)
 
     def __unicode__(self):
         """Return comment of the post."""
-        return  'id: %s date: %s %s = ' % (self.id, str(self.date), str(self.time))
-            
+        return 'id: %s date: %s %s = ' % (self.id, str(self.date), str(self.time))
+
     @classmethod
     def all(cls):
         """Return all posts."""
