3c1bb3dcf4e1337f995da6c5cda0a7c706a7f293
Lucas <lucassalves65@gmail.com>
2015-06-17 21:51:26 -0300

    removed config files again
    Signed-off-by: Lucas <lucassalves65@gmail.com>

--- configuration/security.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Security configurations
-
-SECRET_KEY = '1qaz2wsx'
-LOGIN_URL = '/login/'
-DEBUG = False
-TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
