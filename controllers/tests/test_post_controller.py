663df84ffc15cbde1982297b2acdcddf9cf5a6a4
Lucas <lucassalves65@gmail.com>
2015-06-22 23:17:09 -0300

    tests for post model
    Signed-off-by: Lucas <lucassalves65@gmail.com>

--- controllers/tests/test_post_controller.py
+++ controllers/tests/test_post_controller.py
@@ -3,6 +3,8 @@ from models.post import Post
 from controllers import post_controller
 from exception.api import ApiException
 from models.user import User
+from models.busline import Busline
+from models.terminal import Terminal
 
 
 STATUS_OK = 200
@@ -13,6 +15,9 @@ class PostControllerTest (SimpleTestCase):
 
     def setUp(self):
         self.client = Client()
+        Terminal.objects.all().delete()
+        Busline.objects.all().delete()
+        User.objects.all().delete()
 
     def create_user(self, username):
         self.user = User()
@@ -21,6 +26,19 @@ class PostControllerTest (SimpleTestCase):
         self.user.name = 'test_name'
         self.user.email = 'test@email.tes'
         self.user.save()
+
+    def create_busline(self):
+        busline = Busline()
+        busline.line_number = "001"
+        busline.description = "description"
+        busline.via = "via"
+        busline.route_size = 2.5
+        busline.fee = 2.0
+        terminal = Terminal(description="terminal")
+        terminal.save()
+        busline.save()
+        busline.terminals.add(terminal)
+        return busline
 
     def post_data(self, codigo_latitude, codigo_longitude, review):
         data = {'capacity': '5',
@@ -46,7 +64,7 @@ class PostControllerTest (SimpleTestCase):
         self.client.logout()
         self.user.delete()
 
-    def test_make_post_action_page_with_user(self):
+    def test_make_post_action_with_user(self):
         self.create_user('username-post-1')
         self.client.login(username='username-post-1', password='test_password')
         response = self.client.post(
@@ -55,9 +73,20 @@ class PostControllerTest (SimpleTestCase):
         self.client.logout()
         self.user.delete()
 
+    def test_make_post_action_with_user_and_line(self):
+        busline = self.create_busline()
+        self.create_user('username-post-2')
+        self.client.login(username='username-post-2', password='test_password')
+        response = self.client.post(
+            "/realizar_post/", self.post_data('0', '0', '0'))
+        self.assertEquals(response.status_code, STATUS_OK)
+        self.client.logout()
+        self.user.delete()
+        busline.delete()
+
     def test_make_post_action_page_no_review(self):
-        self.create_user('username-post-1')
-        self.client.login(username='username-post-1', password='test_password')
+        self.create_user('username-post-3')
+        self.client.login(username='username-post-3', password='test_password')
         response = self.client.post(
             "/realizar_post/", self.post_data('0', '0', ''))
         self.assertEquals(response.status_code, STATUS_OK)
@@ -65,8 +94,8 @@ class PostControllerTest (SimpleTestCase):
         self.user.delete()
 
     def test_make_post_action_page_no_geolocation(self):
-        self.create_user('username-post-1')
-        self.client.login(username='username-post-1', password='test_password')
+        self.create_user('username-post-4')
+        self.client.login(username='username-post-4', password='test_password')
         response = self.client.post(
             "/realizar_post/", self.post_data('', '', '0'))
         self.assertEquals(response.status_code, STATUS_OK)
--- models/tests/test_post.py
+++ models/tests/test_post.py
@@ -5,16 +5,20 @@ from models.busline import Busline
 from models.user import User
 from models.terminal import Terminal
 from models.post import Post
+from exception.api import ApiException
 
 
 class BuslineTest(SimpleTestCase):
 
     def setUp(self):
+        Terminal.objects.all().delete()
         Busline.objects.all().delete()
+        User.objects.all().delete()
+        Post.objects.all().delete()
 
-    def create_busline(self):
+    def create_busline(self, line_number):
         busline = Busline()
-        busline.line_number = "001"
+        busline.line_number = line_number
         busline.description = "description"
         busline.via = "via"
         busline.route_size = 2.5
@@ -23,20 +27,25 @@ class BuslineTest(SimpleTestCase):
         terminal.save()
         busline.save()
         busline.terminals.add(terminal)
+        self.busline_id = busline.id
         return busline
 
-    def create_user(self):
+    def create_user(self, username):
         user = User()
-        user.username = 'test_username'
+        user.username = username
         user.password = 'test_password'
         user.name = 'test_name'
         user.email = 'test@email.tes'
         user.save()
         return user
 
-    def create_post(self):
-        busline = self.create_busline()
-        user = self.create_user()
+    def create_post(self, line_number, username):
+        try:
+            busline = self.create_busline(line_number)
+            self.busline_same = busline
+        except:
+            busline = self.busline_same
+        user = self.create_user(username)
         post = Post()
         post.comment = 'comentario'
         post.latitude = '0'
@@ -51,6 +60,42 @@ class BuslineTest(SimpleTestCase):
         return post
 
     def test_post_all(self):
-        self.create_post()
+        self.create_post('001', 'username')
         posts = Post.all()
         self.assertEquals(1, len(posts))
+
+    def test_post_instance(self):
+        post = Post()
+        self.assertIsNotNone(post)
+
+    def test_busline_unicode(self):
+        post = self.create_post('001', 'username')
+        id = str(post.id)
+        date = str(post.date)
+        time = str(post.time)
+        busline_id = str(post.busline.id)
+        self.assertEquals(
+            "id: " + id + " date: " + date + " " + time + " busline: " +
+            busline_id + "", post.__unicode__())
+
+    def test_post_all_empty(self):
+        post = Post.all()
+        self.assertEquals(0, len(post))
+
+    def test_all_exception(self):
+        Post.all()
+        self.assertRaises(ApiException)
+
+    def test_post_last_equals(self):
+        self.create_post('001', 'username')
+        post1 = Post.last(self.busline_id)
+        self.create_post('001', 'username2')
+        post2 = Post.last(self.busline_id)
+        self.assertEquals(post1.busline, post2.busline)
+
+    def test_post_last_differs(self):
+        self.create_post('001', 'username')
+        post1 = Post.last(self.busline_id)
+        self.create_post('002', 'username2')
+        post2 = Post.last(self.busline_id)
+        self.assertNotEqual(post1.busline, post2.busline)
