c77cb8cf3d62b7ae26f71a1ce24a8ceb5df3dd78
Hale Valente <halegal@gmail.com>
2015-06-24 01:40:27 -0300

    controllers changes for the feed page response
    Signed-off-by: Lucas <lucassalves65@gmail.com>
    Signed-off-by: Hale Valente <halegal@gmail.com>

--- controllers/utils.py
+++ controllers/utils.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from models.post import Post
+from models.favorite import Favorite
 
 
 def modal_message(alert_title, error_lead, error_message_in, html, request):
@@ -13,6 +15,24 @@ def modal_message(alert_title, error_lead, error_message_in, html, request):
     htmlvars["modal_message"] = error_message_in
     response = response_htmlvars(htmlvars, html, request)
     return response
+
+
+def generic_htmlvars(html, request, **kvargs):
+    """Populate a htmlvars with generic inputs"""
+    return response_htmlvars(kvargs, html, request)
+
+
+def call_feed_page(request, **kvargs):
+    posts = Post.all()
+    posts = sorted(
+        posts, key=lambda post: (post.time), reverse=True)
+    posts = posts[:10]
+    favorites = Favorite.objects.filter(
+        user_id=request.user)
+    if favorites == []:
+        favorites = None
+    return generic_htmlvars(
+        'feed_page.html', request, posts=posts, favorites=favorites, **kvargs)
 
 
 def response_htmlvars(htmlvars, html, request):
